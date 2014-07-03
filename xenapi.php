072b4f2c7b609a56959754c25acba641ecbecf92
TimIgoe <tim@igoe.me.uk>
2014-07-03 22:19:06 +0100

    Create Composor and base file for XenAPI

---
+++ xenapi.php
@@ -0,0 +1,15 @@
+<?
+
+/**
+ * The main class for accessing the XenServer XMLRPC API
+ */
+ 
+ namespace TSIHosting;
+ 
+ class XenApi 
+ {
+   private function dispatch($Request)
+   {
+     
+   }
+ }
